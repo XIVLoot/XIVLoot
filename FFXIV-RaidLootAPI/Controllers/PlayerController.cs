0186b21badc30515ef150eb50e8a67a2e3dad3ce
Anthony Desrochers <anthony.desrochers17@gmail.com>
2024-05-20 12:34:47 -0400

    Created PlayerDTO

--- FFXIV-RaidLootAPI/Controllers/PlayerController.cs
+++ FFXIV-RaidLootAPI/Controllers/PlayerController.cs
@@ -1,4 +1,5 @@
 using FFXIV_RaidLootAPI.Data;
+using FFXIV_RaidLootAPI.DTO;
 using FFXIV_RaidLootAPI.Models;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
@@ -28,78 +29,78 @@ namespace FFXIV_RaidLootAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<int>> GetAverageLevel(int Id, bool UseBis)
+        public async Task<ActionResult<int>> GetAverageLevel(PlayerDTO dto)
         {
-            Players? player = await _context.Players.FindAsync(Id);
+            Players? player = await _context.Players.FindAsync(dto.Id);
             if (player is null)
                 return NotFound("Player is not found.");
 
-            return player.get_avg_item_level(null,UseBis,_context);
+            return player.get_avg_item_level(null,dto.UseBis,_context);
         }
 
         // POST
 
         [HttpPut]
-        public async Task<ActionResult> UpdatePlayerGear(int Id,GearType GearToChange, int NewGearId, bool Usebis)
+        public async Task<ActionResult> UpdatePlayerGear(PlayerDTO dto)
         {// Changes only one of the gear type. Changes the given GearToChange to NewGearid. if Usebis set to true
          // changes the value for the bis gear set of the player.
 
-        Players? player = await _context.Players.FindAsync(Id);
+        Players? player = await _context.Players.FindAsync(dto.Id);
         if (player is null)
             return NotFound("Player not found");
-        player.change_gear_piece(GearToChange,Usebis,NewGearId);
+        player.change_gear_piece(dto.GearToChange,dto.UseBis,dto.NewGearId);
         _context.SaveChanges();
         return Ok();
         }
 
         [HttpPut]
-        public async Task<ActionResult> UpdatePlayerEtro(int Id,string NewEtro)
+        public async Task<ActionResult> UpdatePlayerEtro(PlayerDTO dto)
         {// Changes only one of the gear type. Changes the given GearToChange to NewGearid. if Usebis set to true
          // changes the value for the bis gear set of the player.
 
-        Players? player = await _context.Players.FindAsync(Id);
+        Players? player = await _context.Players.FindAsync(dto.Id);
         if (player is null)
             return NotFound("Player not found");
-        player.EtroBiS = NewEtro;
+        player.EtroBiS = dto.NewEtro;
         _context.SaveChanges();
         return Ok();
         }
 
         [HttpPut]
-        public async Task<ActionResult> UpdateName(int Id,string NewName)
+        public async Task<ActionResult> UpdateName(PlayerDTO dto)
         {// Changes only one of the gear type. Changes the given GearToChange to NewGearid. if Usebis set to true
          // changes the value for the bis gear set of the player.
 
-        Players? player = await _context.Players.FindAsync(Id);
+        Players? player = await _context.Players.FindAsync(dto.Id);
         if (player is null)
             return NotFound("Player not found");
-        player.Name = NewName;
+        player.Name = dto.NewName;
         _context.SaveChanges();
         return Ok();
         }
 
         [HttpPut]
-        public async Task<ActionResult> UpdateJob(int Id,Job job)
+        public async Task<ActionResult> UpdateJob(PlayerDTO dto)
         {// Changes only one of the gear type. Changes the given GearToChange to NewGearid. if Usebis set to true
          // changes the value for the bis gear set of the player.
 
-        Players? player = await _context.Players.FindAsync(Id);
+        Players? player = await _context.Players.FindAsync(dto.Id);
         if (player is null)
             return NotFound("Player not found");
-        player.Job = job;
+        player.Job = dto.NewJob;
         _context.SaveChanges();
         return Ok();
         }
 
         [HttpPut]
-        public async Task<ActionResult> UpdateLocked(int Id,bool NewLock)
+        public async Task<ActionResult> UpdateLocked(PlayerDTO dto)
         {// Changes only one of the gear type. Changes the given GearToChange to NewGearid. if Usebis set to true
          // changes the value for the bis gear set of the player.
 
-        Players? player = await _context.Players.FindAsync(Id);
+        Players? player = await _context.Players.FindAsync(dto.Id);
         if (player is null)
             return NotFound("Player not found");
-        player.Locked = NewLock;
+        player.Locked = dto.NewLock;
         _context.SaveChanges();
         return Ok();
         }
