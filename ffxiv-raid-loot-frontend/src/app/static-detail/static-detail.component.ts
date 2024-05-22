29290714a7f9e89d2cf232bce3d7b3e02b993d2b
Ricky Chittaphone <rickychittaphone@gmail.com>
2024-05-22 14:34:38 -0400

    Added some basic styling to the pages
    Added some basic styling to the pages

--- ffxiv-raid-loot-frontend/src/app/static-detail/static-detail.component.ts
+++ ffxiv-raid-loot-frontend/src/app/static-detail/static-detail.component.ts
@@ -1,28 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { Static } from '../models/static';
-import { HttpService } from '../service/http.service';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Static } from '../models/static'; // Importing the Static model
+import { HttpService } from '../service/http.service'; // Importing the HttpService
+import { ActivatedRoute } from '@angular/router'; // Importing ActivatedRoute to access route parameters
 
 @Component({
-  selector: 'app-static-detail',
-  templateUrl: './static-detail.component.html',
-  styleUrls: ['./static-detail.component.css']
+  selector: 'app-static-detail', // Component selector used in HTML
+  templateUrl: './static-detail.component.html', // HTML template for the component
+  styleUrls: ['./static-detail.component.css'] // Stylesheet for the component
 })
 export class StaticDetailComponent implements OnInit {
-  public staticDetail: Static;
-  uuid: string;
+  public staticDetail: Static; // Holds the details of a static
+  uuid: string; // UUID of the static
+  gridColumns = 4; // Default number of grid columns
 
-  constructor(public http: HttpService, private route: ActivatedRoute) { }
+  constructor(public http: HttpService, private route: ActivatedRoute) { } // Constructor with dependency injection
 
   ngOnInit(): void {
+    // Subscribe to route parameters to get the 'uuid'
     this.route.params.subscribe(params => {
       this.uuid = params['uuid'];
     });
+    // Fetch static details from the server using the uuid
     this.http.getStatic(this.uuid).subscribe(details => {
-      this.staticDetail = details;
-      console.log(this.staticDetail);
+      this.staticDetail = details; // Assign the fetched details to staticDetail
+      console.log(this.staticDetail); // Log the static details to the console
     });
+    this.onResize(null); // Call onResize to set initial gridColumns based on window size
+  }
+
+  @HostListener('window:resize', ['$event']) // Listen to window resize events
+  onResize(event) {
+    // Adjust gridColumns based on window width
+    if (window.innerWidth < 600) {
+      this.gridColumns = 1;
+    } else if (window.innerWidth >= 600 && window.innerWidth < 768) {
+      this.gridColumns = 2;
+    } else if (window.innerWidth >= 768 && window.innerWidth < 992) {
+      this.gridColumns = 3;
+    } else {
+      this.gridColumns = 4;
+    }
   }
 }
-
 
