a3790089ed69cbdd3051c102317a9bac6aa1cd4f
Ricky Chittaphone <rickychittaphone@gmail.com>
2024-06-19 00:51:24 -0400

    Fixed env variables
    Fixed env variables

--- ffxiv-raid-loot-frontend/src/environments/environments.prod.ts
+++ ffxiv-raid-loot-frontend/src/environments/environments.prod.ts
@@ -2,8 +2,8 @@
 
   export const environment = {
     production: true,
-    site_url : "https://xivloot.shiraki.ca/",
-    api_url : "https://api.shiraki.ca/",
-    discord_redirect_url : "https://discord.com/oauth2/authorize?client_id=1252372424138166343&response_type=code&redirect_uri=https%3A%2F%2Fxivloot.shiraki.ca%2Fauth%2Fdiscord%2Fcallback&scope=identify"
+    site_url : "https://xivloot.com/",
+    api_url : "https://api.xivloot.com/api/",
+    discord_redirect_url : "https://discord.com/oauth2/authorize?client_id=1252372424138166343&response_type=code&redirect_uri=https%3A%2F%2Fxivloot.com%2Fauth%2Fdiscord%2Fcallback&scope=identify"
   };
   
